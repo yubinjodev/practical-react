ff7080b55ac1a6945c7ace98bde5f7a20b83109b
yubin <yubin@itdadev.com>
2022-01-20 16:07:16 +0900

    i have a domain name now

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1>Welcome to Yubin's version of practical-react</h1>)
+        <h1>Welcome to Yubin's Portfolio</h1>)
         <div className="nav-container">
           <h3>My components</h3>
           <Link to="imageSlider">Image Slider</Link>
