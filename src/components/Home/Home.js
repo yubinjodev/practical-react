7334beeb7ec056b31881cf57d6d43bb068096f10
yubin <yubin@itdadev.com>
2022-01-30 02:15:46 +0900

    created projects page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,46 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Home.css";
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <div className="home">
-        <div className="home-container">
-          <h3>React Components</h3>
-          <Link className="components" to="imageSlider">
-            Image Slider
-          </Link>
-
-          <Link className="components" to="counter">
-            Counter
-          </Link>
-
-          <Link className="components" to="myForm">
-            My Form
-          </Link>
-
-          <Link className="components" to="validationForm">
-            Validation Form
-          </Link>
-
-          <Link className="components" to="fetchRandomUser">
-            Fetch Random User
-          </Link>
-
-          <Link className="components" to="fetchMultiple">
-            Fetch Multiple
-          </Link>
-
-          <Link className="components" to="syncCounter">
-            Sync Counter
-          </Link>
-
-          <Link className="components" to="todoList">
-            Todo List
-          </Link>
-        </div>
-      </div>
-    );
-  }
+export default function Home() {
+  return <h1 className="homescreen">Welcome to Yubin's Portfolio</h1>;
 }
