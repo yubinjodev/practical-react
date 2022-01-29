00c929ba8e5298a758c9fccbce47b959b59b7378
yubin <yubin@itdadev.com>
2022-01-30 02:13:09 +0900

    navbar improvements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component, useEffect, useState } from "react";
 import Loading from "./components/Loading/Loading";
-import Menu from "./Menu";
 import Header from "./components/Header/Header";
-import BounceLoader from "react-spinners/BounceLoader";
 import "./App.css";
-import Home from "./components/Home/Home";
 
 class App extends Component {
   componentDidMount() {
@@ -18,17 +15,7 @@ class App extends Component {
       this.setState({ loading: false });
     }, 2000);
 
-    return (
-      <div>
-        {this.state.loading ? <div></div> : <Header />}
-        {this.state.loading ? <Loading /> : <div></div>}
-        {this.state.loading ? (
-          <div></div>
-        ) : (
-          <h1>Welcome to Yubin's Portfolio</h1>
-        )}
-      </div>
-    );
+    return <div>{this.state.loading ? <Loading /> : <Header />}</div>;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,35 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+import { Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import Projects from "./components/Projects/Projects";
+import ImageSlider from "./components/ImageSlider/ImageSlider";
+import Counter from "./components/Counter/Counter";
+import MyForm from "./components/MyForm/MyForm";
+import ValidationForm from "./components/ValidationForm/ValidationForm";
+import FetchRandomUser from "./components/FetchRandomUser/FetchRandomUser";
+import FetchMultiple from "./components/FetchMultiple/FetchMultiple";
+import SyncCounter from "./components/SyncCounter/SyncCounter";
+import TodoList from "./components/TodoList/TodoList";
+import Home from "./components/Home/Home";
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/imageSlider" element={<ImageSlider />} />
+      <Route path="/projects/counter" element={<Counter />} />
+      <Route path="/projects/myForm" element={<MyForm />} />
+      <Route path="/projects/validationForm" element={<ValidationForm />} />
+      <Route path="/projects/fetchRandomUser" element={<FetchRandomUser />} />
+      <Route path="/projects/fetchMultiple" element={<FetchMultiple />} />
+      <Route path="/projects/syncCounter" element={<SyncCounter />} />
+      <Route path="/projects/todoList" element={<TodoList />} />
+    </Routes>
   </BrowserRouter>,
   document.getElementById("root")
 );
