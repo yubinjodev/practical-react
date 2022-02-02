9df4bf61fe55a2f7eb2a0053d916f02285363920
yubin <yubin@itdadev.com>
2022-02-02 15:08:18 +0900

    added new art gallery item

--- src/components/Projects/ArtGallery/ArtGallery.js
+++ src/components/Projects/ArtGallery/ArtGallery.js
@@ -3,6 +3,7 @@ import Flowers from "./Images/flowers.png";
 import Reflection from "./Images/reflection.png";
 import Bubble from "./Images/bubble.jpg";
 import Sunflowers from "./Images/sunflowers.jpg";
+import Saturn from "./Images/saturn.png";
 
 export default class ArtGallery extends React.Component {
   render() {
@@ -12,6 +13,7 @@ export default class ArtGallery extends React.Component {
         <img className="frame " src={Reflection} />
         <img className="frame " src={Bubble} />
         <img className="frame " src={Sunflowers} />
+        <img className="frame " src={Saturn} />
       </div>
     );
   }
