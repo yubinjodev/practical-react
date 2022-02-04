dbd8188d552c0bd95f2dca086c7c6db82f826b53
yubin <yubin@itdadev.com>
2022-02-04 18:41:00 +0900

    setting up firedb config to test geocoding.

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,24 +1,26 @@
 import React from "react";
-import {
-  Circle,
-  Ellipse,
-  GroundOverlay,
-  Marker,
-  NaverMap,
-  Overlay,
-  Polygon,
-  Polyline,
-  Rectangle,
-  RenderAfterNavermapsLoaded,
-  loadNavermapsScript,
-} from "react-naver-maps";
-import { Geocoding } from "./Geocoding";
 import NaverMapApi from "./NaverMapApi";
+import { useNavigate, useParams } from "react-router-dom";
+import { Geocoding } from "./Geocoding";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import firebaseDB from "../../firebaseConfig";
 import "./AboutMe.css";
 
 export default class AboutMe extends React.Component {
   state = {
-    query: "",
+    name: "",
+    adress: "",
+  };
+  submitHandle = () => {
+    firebase.child("Farm").push(this.state, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        alert("Address added!");
+      }
+    });
   };
 
   render() {
@@ -28,19 +30,29 @@ export default class AboutMe extends React.Component {
         <h3>{this.state.map}</h3>
         <div className="map-container">
           <input
-            placeholder="address"
-            value={this.state.query}
+            placeholder="name"
+            value={this.state.name}
             onChange={(e) => {
               this.setState({
-                query: e.target.value,
+                name: e.target.value,
+              });
+            }}
+          ></input>
+          <input
+            placeholder="address"
+            value={this.state.adress}
+            onChange={(e) => {
+              this.setState({
+                adress: e.target.value,
               });
             }}
           ></input>
 
           <button
-            onClick={() => {
-              Geocoding(`${this.state.query}`);
-            }}
+            onClick={() => this.submitHandle}
+            // onClick={() => {
+            //   Geocoding(`${this.state.adress}`);
+            // }}
             className="map-button"
           >
             search
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5shDwXQm5HNhUdG52vWVYExG6OWkAzlU",
+  authDomain: "portfolio-61c99.firebaseapp.com",
+  databaseURL: "https://portfolio-61c99-default-rtdb.firebaseio.com",
+  projectId: "portfolio-61c99",
+  storageBucket: "portfolio-61c99.appspot.com",
+  messagingSenderId: "827726660411",
+  appId: "1:827726660411:web:3e9ec6f14597ec53fead1e",
+};
+
+const firebaseDB = firebase.initializeApp(firebaseConfig);
+
+export default firebaseDB.database().ref();
